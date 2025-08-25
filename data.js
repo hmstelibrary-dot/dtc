97a375f172cf2ea3e3b11dd0d6149b0b7b10567d
hmstelibrary-dot <hmst.elibrary@gmail.com>
2025-08-25 16:24:27 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    "brand": "HINO",
+    "model": "300-INNOVATOR",
+    "variant": "XZU(N04C)",
+    "codes": [
+      {
+        "code": "B2799",
+        "description": "Immobilizer system: ระบบ Imobilizer \nการสื่อสารของ ECU ที่แยกจากระบบ Immobilizer ผิดปกติ, เครื่องยนต์ไม่สามารถสตาร์ทได้",
+        "possible_causes": [
+          "ตรวจพบความผิดปกติในการสื่อสารหรือสายสื่อสารระหว่าง ECU ของเครื่องยนต์และ ECU ของระบบ immobilizer",
+          "ตรวจพบรหัสการสื่อสารที่ไม่ถูกต้อง"
+        ],
+        "diagnosis_steps": [
+          "ยังไม่ได้ลงทะเบียนรหัส ID สำหรับกุญแจสตาร์ท (การลงทะเบียนไม่เสร็จสมบูรณ์)",
+          "คอนเนคเตอร์เชื่อมต่อเกิดการหลุดหลวม",
+          "สายไฟขาดหรือลัดวงจร",
+          "Immobilizer ECU ทำงานบกพร่อง",
+          "Engine ECU ทำงานบกพร่อง"
+        ],
+        "repair_steps": [
+          "ลงทะเบียนรหัส ID ให้เสร็จสมบูรณ์",
+          "แก้ไขขั้วต่อต่างๆ อย่างถูกต้อง",
+          "ซ่อมแก้ไขหรือเปลี่ยนสายไฟหากพบเจอความเสียหาย",
+          "เปลี่ยน Engine ECU หรือ Immobilizer ECU หากตรวจสอบแล้วว่าทำงานบกพร่อง"
+        ],
+        "reference": "Manual: S7-LXZE05I 1/4 [Page 2-24 to 2-30]",
+        "images": [
+          "https://hmstelibrary-dot.github.io/Photo/XZU(N04C)-B2799-S7-LXZE05I_11.jpg",
+          "https://hmstelibrary-dot.github.io/Photo/XZU(N04C)-B2799-S7-LXZE05I_12.jpg",
+          "https://hmstelibrary-dot.github.io/Photo/XZU(N04C)-B2799-S7-LXZE05I_13.jpg",
+          "https://hmstelibrary-dot.github.io/Photo/XZU(N04C)-B2799-S7-LXZE05I_14.jpg",
+          "https://hmstelibrary-dot.github.io/Photo/XZU(N04C)-B2799-S7-LXZE05I_15.jpg",
+          "https://hmstelibrary-dot.github.io/Photo/XZU(N04C)-B2799-S7-LXZE05I_16.jpg",
+          "https://hmstelibrary-dot.github.io/Photo/XZU(N04C)-B2799-S7-LXZE05I_17.jpg"
+        ]
+      }
+    ]
+  }
+];
